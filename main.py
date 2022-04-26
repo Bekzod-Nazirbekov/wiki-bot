cdd76c862ea66053832cdc524a0ecb016b004c3d
Bekzod-Nazirbekov <99839920+Bekzod-Nazirbekov@users.noreply.github.com>
2022-04-26 15:23:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import logging
+import wikipedia
+
+from aiogram import Bot, Dispatcher, executor, types
+
+wikipedia.set_lang('uz')
+
+API_TOKEN = '5208297006:AAHlSQXHNc2307sbkPnmis_1G66SPTtu-4A'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Assalomu alaykum Wikipedia\nbotga xush kelibsiz!")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+    try:
+        response = wikipedia.summary(message.text)
+        await message.answer(response)
+    except:
+        await message.answer("Bunday malumot topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
